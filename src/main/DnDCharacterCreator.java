d8ee0f73f31efe428757ade220a850c593861e85
KingOfSpadesJFK <kingofspadeslm567@gmail.com>
2022-04-21 23:55:51 -0700

    moved main.java into dndcharactercreator.java and characterbuild.java into src/main/

--- src/main/DnDCharacterCreator.java
+++ src/main/DnDCharacterCreator.java
@@ -4,17 +4,53 @@
  */
 package main;
 
+import java.util.*;
+import java.util.Scanner;
+
 /**
  *
- * @author Hunter
+ * @author Hunter, Zach Holloway
  */
 public class DnDCharacterCreator {
 
     private static Menu menu;
+
+    public static void main(String[] args)
+    {
+        // sets up a scanner to take user inputw
+        Scanner input = new Scanner(System.in);
+        CharacterBuild testBuild = new CharacterBuild();
+
+        System.out.println("""
+                           Preparing to create Character
+                           Do you wish to roll for stats, or use Point Buy?
+                           (Type 'R' for Roll, 'P' for Point Buy, or 'Q' to Quit)""");
+        String part1 = input.nextLine();
+        switch(part1){
+            case "R", "r" -> {
+                System.out.println("Getting ready to roll!");
+                testBuild.rollDice();
+                System.out.println("""
+                                   How would you like to distribute these points?
+                                   (Randomly(R) or Selected(S)?)""");
+                String pickOrRand = input.nextLine();
+                testBuild.statSelect(pickOrRand);
+            }
+            case "P", "p" -> {
+                System.out.println("Getting the points ready!");
+                testBuild.pointBuy();
+            }
+            case "Q", "q" -> {
+                System.out.println("Exiting...");
+                System.exit(0);
+            }
+        }
+    }
+
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    private static void runMenu() {
         // TODO code application logic here
         menu = new Menu();
         
