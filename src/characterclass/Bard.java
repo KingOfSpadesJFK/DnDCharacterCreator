4ef672c32e8a0f6f0adb290072292f460aa1a729
Zachery Holloway <104041044+HolloZ@users.noreply.github.com>
2022-05-10 22:56:08 -0700

    Create Bard.java
    Started making the bard to try and get my spell idea working

---
+++ src/characterclass/Bard.java
@@ -0,0 +1,26 @@
+package characterclass;
+import java.util.ArrayList;
+import java.util.List;
+import main.GameCharacter.Ability;
+
+public class Bard extends CharacterClass{
+	int constitutionModifier;
+	int proficiencyBonus;
+	int hitPoints = (constitutionModifier + 8);
+	int hitDie = 1;
+	int hitDice = 8;
+
+	public enum Skills{
+		//
+	};
+
+	public enmu Features{
+		//
+	};
+
+	public Bard(int[] hitDie, Ability[] primaryAbility, Ability[] savingThrows, 
+				ArmorType[] armorProficiencies, WeaponType[] weaponProficiencies) {
+		super(hitDie, primaryAbility, savingThrows, armorProficiencies, weaponProficiencies);
+	}
+	
+}
