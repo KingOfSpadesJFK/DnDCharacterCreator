6cbe9fa63da37a202a5cb98116045d6a6d66907f
That1trumpetguy <104122775+That1trumpetguy@users.noreply.github.com>
2022-06-07 01:56:22 -0700

    Fully Fleshed out the Elf Race and Sub-Races
    Added traits and changed the ASI's to better be in line with the rest of the other races

--- src/race/Elf.java
+++ src/race/Elf.java
@@ -1,42 +1,119 @@
 package race;
 
-class Elf extends Race
-{
+import main.GameCharacter.Language;
+import main.GameCharacter.Size;
 
+public class Elf extends Race
+{
     public final static String RACE_NAME = "Elf";
 
-    public Elf(int[] stats)
+    private final static Language[] languages =
+            {
+                    Languages.COMMON,
+                    Languages.ELVISH
+            };
+
+    private static final String[] traits = {
+            "Tance",
+            "Keen Senses",
+            "Fey Ancestry"
+    };
+
+    public Elf()
     {
-        super(RACE_NAME, 0, 2, 0, 0, 0, 0);
+        super(RACE_NAME, 0, 2, 0, 0 , 0, 0);
+        setTraits(traits);
+        setLanguages(languages);
+        setSize(Size.MEDIUM);
+        seSpeed(30);
     }
 
+    private Elf(String[] traits, String race, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
+    {
+        super(race, strength, dexterity, constitution, intelligence, wisdom, charisma);
+        setTraits(traits);
+        setLanguages(languages);
+        setSize(Size.MEDIUM);
+        setSpeed(30);
+    }
 }
-class High_Elf extends Race
+
+public class High_Elf extends Elf
 {
-    public final static String RACE_NAME = "High_Elf";
+    public static final String RACE_NAME = "High_Elf";
+    private static final Language[] languages =
+            {
+                    Languages.COMMON,
+                    Languages.ELVISH,
+                    Languages.SELECT,
+                    Languages.EXTRA_SELECT
+            };
 
-    public High_Elf(int[] stats)
+    private static final String[] traits = {
+            "Tance",
+            "Keen Senses",
+            "Fey Ancestry",
+            "Elf Weapon Training",
+            "Cantrip",
+            "Ability Score Increase"
+    };
+
+    public High_Elf()
     {
-        super(RACE_NAME, 1, 2, 0, 0, 0, 0);
+        super(traits, RACE_NAME, 0, 2, 0, 1, 0 , 0);
     }
+
 }
 
-class Drow extends Race
+class Drow extends Elf
 {
     public final static String RACE_NAME = "Drow";
-
-    public Drow(int[] stats)
+    public Drow()
     {
-        super(RACE_NAME, 0, 2, 0, 0, 0, 1);
+        super(traits, RACE_NAME, 0, 2, 0, 0, 0, 1);
     }
+    private final static Language[] languages =
+            {
+                    Languages.COMMON,
+                    Languages.ELVISH,
+                    Languages.SELECT
+            };
+
+    private static final String[] traits = {
+            "Tance",
+            "Keen Senses",
+            "Fey Ancestry",
+            "Drow Weapon Training",
+            "Drow Magic",
+            "Sunlight Sensitivity",
+            "Superior Darkvision",
+            "Ability Score Increase"
+
+    };
 }
 
-class Wood_Elf extends Race
+class Wood_Elf extends Elf
 {
     public final static String RACE_NAME = "Wood_Elf";
-
-    public Wood_Elf(int[] stats)
+    public Wood_Elf()
     {
-        super(RACE_NAME, 0, 2, 0, 0, 1, 0);
+        super(traits, RACE_NAME, 0, 2, 0, 0, 1, 0);
     }
+    private final static Language[] languages =
+            {
+                    Languages.COMMON,
+                    Languages.ELVISH,
+                    Languages.SELECT
+            };
+
+    private static final String[] traits = {
+            "Tance",
+            "Keen Senses",
+            "Fey Ancestry",
+            "Elf Weapon Training",
+            "Fleet of Foot",
+            "Mask of the Wild",
+            "Ability Score Increase"
+    };
+
 }
